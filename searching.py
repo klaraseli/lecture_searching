2aa43bbefb22728d890609609c2415561e154f88
Selingrová Klára (257041) <257041@vutbr.cz>
2024-04-08 12:38:59 +0200

    linear_searching

--- searching.py
+++ searching.py
@@ -22,9 +22,23 @@ def read_data(file_name, field):
                 return value
         return None
 
+def linear_search(sekvence, hledane_cislo):
+    vyskyty = 0
+    pozice = []
+    for idx, cislo in enumerate(sekvence):
+        if cislo == hledane_cislo:
+            vyskyty = vyskyty + 1
+            pozice.append(idx)
+    slovnik = {"pocet vyskytu" : vyskyty, "pozice vyskytu" : pozice}
+    return slovnik
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
+    cislo = 5
+    slovnik = linear_search(sequential_data, cislo)
+    print(slovnik)
 
 
 if __name__ == '__main__':
